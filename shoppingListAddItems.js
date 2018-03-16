84eea1d139580df816d14c8cdc100959c25a74eb
Giorvi Alvarez <giorvialvarez@Giorvis-MacBook-Pro.local>
2018-03-16 08:43:29 -0700

    idk what u did

---
+++ shoppingListAddItems.js
@@ -0,0 +1,17 @@
+ var submitButton = document.getElementById('submit')
+ submitButton.addEventListener("click", () => {
+     document.getElementById('list').textContent = addItem();
+ });
+
+ //var itemInput = document.getElementById("item");
+ //var clear = document.getElementById("clear");
+ //var submit = document.getElementById("submit");
+ // var list = document.getElementById("list");
+
+ function addItem() {
+     var itm = document.getElementById('item');
+     var addItm = document.createElement('list');
+     addItm.textContent = ('item')
+ }
+
+
