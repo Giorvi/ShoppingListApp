641013ffec21a533bc0275d4070f1f6e98cbbc99
Giorvi Alvarez <giorvialvarez@Giorvis-MacBook-Pro.local>
2018-03-23 11:55:12 -0700

    it still doesnt work but im commiting

--- shoppingListAddItems.js
+++ shoppingListAddItems.js
@@ -1,17 +1,19 @@
  var submitButton = document.getElementById('submit');
  submitButton.addEventListener("click", () => {
-     document.getElementById('list').textContent = addItem();
+     document.getElementById('').textContent = addItem();
  });
 
- //var itemInput = document.getElementById("item");
- //var clear = document.getElementById("clear");
- //var submit = document.getElementById("submit");
- // var list = document.getElementById("list");
+function addItem() {
+    var itemInput = document.getElementById("item");
+    var newItem = document.createElement("li");
+    var itemcontent = document.createTextNode(itemInput);
 
- function addItem() {
-     var itm = document.getElementById('item');
-     var addItm = document.createElement('list');
-     addItm.textContent = ('item');
- }
+    newItem.appendChild(itemcontent);
+
+    var currentItem = document.getElementById("item");
+    document.body.insertBefore(newItem, currentItem);
+
+
+}
 
 
