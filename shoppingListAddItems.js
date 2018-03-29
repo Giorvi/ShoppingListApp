285e73145d2d852c283232b3d886974fbb564db1
Giorvi Alvarez <giorvialvarez@Giorvis-MacBook-Pro.local>
2018-03-29 10:56:54 -0700

    not working but....

--- shoppingListAddItems.js
+++ shoppingListAddItems.js
@@ -1,15 +1,32 @@
- var submitButton = document.getElementById('submit');
- submitButton.addEventListener("click", addItem);
+var submitButton = document.getElementById('submit');
+button.addEventListener("click", () => {
+    addItem();
+});
 
-function addItem(){
+function addItem() {
+    var deletebutton = document.createElement('button'),
+        input = document.getElementById("input").value,
+        li = document.createElement("li");
 
-    var input = document.getElementById("item").value;
-    var li = document.createElement("li");
+    //     document.getElementById("input").value = "";
 
     li.textContent = input;
-
     return document.getElementById("list").appendChild(li);
 
+
+    /*  deletebutton.textContent = "Delete";
+      deletebutton.className = "delete"
+
+      deletebutton.addEventListener("click", () => {
+         var target = deletebutton.parentElement;
+         target.remove();
+     });*/
+
+    content.textContent = input + " ";
+
+    li.appendChild(content);
+    li.appendChild(deletebutton);
+
+    list.appendChild(li);
+
 }
-
-
