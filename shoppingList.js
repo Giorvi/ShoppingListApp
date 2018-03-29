1e1dbeeb36945bacb257969020d451764c2f9db5
Giorvi Alvarez <giorvialvarez@Giorvis-MacBook-Pro.local>
2018-03-29 11:38:36 -0700

    final

---
+++ shoppingList.js
@@ -0,0 +1,32 @@
+var submitbutton = document.getElementById("submit");
+button.addEventListener("click", () => {
+    addItem();
+});
+
+var list = document.getElementsByTagName("ul")[0];
+
+function addItem() {
+    var input = document.getElementById("item").value;
+    var li = document.createElement("li");
+    var textcontent = document.createElement("empty");
+    var deletebutton = document.createElement("button");
+    var clearbutton = document.getElementById("clear");
+
+    document.getElementById("item").value = ""
+
+    deletebutton.textContent = "Remove Item";
+    deletebutton.className = "delete"
+
+    deletebutton.addEventListener("click", () => {
+        var target = deletebutton.parentElement;
+        target.remove();
+    });
+
+    textcontent.textContent = input;
+
+    li.appendChild(textcontent);
+    li.appendChild(deletebutton);
+
+    list.appendChild(li);
+
+}
